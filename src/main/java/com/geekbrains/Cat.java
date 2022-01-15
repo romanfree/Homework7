f9611f96ddf8f88157feaa3087621392c9fb8924
roman <romanfree1311@gmail.com>
2022-01-15 15:51:39 +0300

    домашка к 7 уроку

--- src/main/java/com/geekbrains/Cat.java
+++ src/main/java/com/geekbrains/Cat.java
@@ -1,4 +1,32 @@
 package com.geekbrains;
 
 public class Cat {
+    private String name;
+    private int appetite;
+    private boolean satiety;
+
+    public Cat(String name, int appetite, boolean satiety){
+        this.name = name;
+        this.appetite = appetite;
+        this.satiety = satiety;
+    }
+
+    public void setAppetite(int appetite) {
+        this.appetite = appetite;
+    }
+    public int getAppetite(){
+        return appetite;
+    }
+
+    public void eat(Plate p){
+        if(p.getFood() >= getAppetite()) {
+            p.decreaseFood(appetite);
+            this.satiety = true;
+            System.out.println(name + " поел(а)");
+        }
+    }
+
+    public void catInfo(){
+        System.out.println(name + " сыт(а): " + satiety);
+    }
 }
--- src/main/java/com/geekbrains/Plate.java
+++ src/main/java/com/geekbrains/Plate.java
@@ -1,4 +1,33 @@
 package com.geekbrains;
 
 public class Plate {
+    private int food;
+
+    public Plate(int food){
+        this.food = food;
+    }
+
+    public void setFood(int food){
+        if(food >= 0) {
+            this.food = food;
+        } else {
+            System.out.println("В тарелке с едой не может получиться отрицательного количества еды");
+        }
+    }
+    public int getFood(){
+        return food;
+    }
+
+    public void decreaseFood(int amount){
+        food -= amount;
+    }
+
+    public void info(){
+        System.out.println("Количество еды: " + food);
+    }
+
+    public void fillPlate(int fillAmount){
+        food += fillAmount;
+        System.out.println("Тарелка пополнилась на: " + fillAmount + "ед.еды");
+    }
 }
